06dbb90b0dabe7c0e9591666855358ad481bf595
JK <viletert@gmail.com>
2024-05-27 14:02:42 +0300

    feat button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Link } from 'react-router-dom'
+
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Button as={Link} to={'https://google.com'}>
+          Hello
+        </Button>
+      </BrowserRouter>
+    </div>
+  )
 }
