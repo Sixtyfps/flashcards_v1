880e301f786543389e43991c1e844ba2c9ad5638
Diana <m.diana.dev@gmail.com>
2024-05-28 20:35:35 +0300

    feat: added dark theme to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,8 +3,22 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '../src/styles/index.scss'
 
+import { themes } from '@storybook/theming'
+
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
