e863f0814163abd150a73626df252d277f1d585c
Chrizzoman <sasuke123451997@gmail.com>
2024-06-10 14:53:23 +0300

    Create Header and refactoring

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,38 +1,39 @@
-import * as Slider from '@radix-ui/react-slider'
-import s from './slider.module.scss'
 import { Typography } from '@/components/ui/typography'
+import * as Slider from '@radix-ui/react-slider'
+
+import s from './slider.module.scss'
 
 export type SliderType = {
-  value: number[]
   max?: number
-  step?: number
   setValue?: (value: number[]) => void
+  step?: number
+  value: number[]
 }
 
 export const SliderApp = (props: SliderType) => {
-  const { value, max = 99, step = 1, setValue } = props
+  const { max = 99, setValue, step = 1, value } = props
 
   return (
     <form>
-      <Typography as={'div'} variant={'body2'} style={{ fontSize: '14px', marginBottom: '5px' }}>
+      <Typography as={'div'} style={{ fontSize: '14px', marginBottom: '5px' }} variant={'body2'}>
         Number of cards
       </Typography>
       <div className={s.sliderContainer}>
-        <Typography as={'div'} variant={'body1'} className={s.sliderValue}>
+        <Typography as={'div'} className={s.sliderValue} variant={'body1'}>
           {value[0]}
         </Typography>
         <Slider.Root
           className={s.SliderRoot}
-          value={value}
-          onValueChange={setValue}
           max={max}
+          onValueChange={setValue}
           step={step}
+          value={value}
         >
           <Slider.Track className={s.SliderTrack}>
             <Slider.Range className={s.SliderRange} />
           </Slider.Track>
-          <Slider.Thumb className={s.SliderThumb} aria-label="Lower value" />
-          <Slider.Thumb className={s.SliderThumb} aria-label="Higher value" />
+          <Slider.Thumb aria-label={'Lower value'} className={s.SliderThumb} />
+          <Slider.Thumb aria-label={'Higher value'} className={s.SliderThumb} />
         </Slider.Root>
         <div className={s.sliderValue}>{value[1]}</div>
       </div>
